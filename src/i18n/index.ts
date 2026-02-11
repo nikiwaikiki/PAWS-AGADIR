aa6c1db15f6eba748dbe435117a2569110dd1daa
nikiwaikiki <niklas.schlichting@googlemail.com>
2026-02-11 21:30:46 +0100

    Add files via upload

---
+++ src/i18n/index.ts
@@ -0,0 +1,34 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import en from './locales/en.json';
+import de from './locales/de.json';
+import fr from './locales/fr.json';
+
+const resources = {
+  en: { translation: en },
+  de: { translation: de },
+  fr: { translation: fr },
+};
+
+// Initialize i18n synchronously before any React code runs
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
+
+export default i18n;
